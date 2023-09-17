a6d30fe5661b28d5f0d39a6ccf94e02875bf2afa
mluron-ArxFjs <ms.luron@nycacademy.org>
2023-09-17 19:59:00 -0400

    updated tmEventController in Server/

--- server/controllers/tmEventController.js
+++ server/controllers/tmEventController.js
@@ -40,7 +40,47 @@ var userInput = function (keyword, startDate, endDate, location, latitude, longi
         }
     };
 }
+$(document).ready(function () {
 
+    $('#btnSubmit').on('click', function (event) {
+        event.preventDefault();
+        var keyword = $("#keyword").val().trim();
+        var startDate = $("#startDate").val().trim();
+        var endDate = $("#endDate").val().trim();
+        var location = $("#location").val().trim();
+        if (localStorage.getItem("windowLatitude") !== null || localStorage.getItem("windowLongitude") !== null) {
+            var currentLatitude = localStorage.getItem("windowLatitude");
+            var currentLongitude = localStorage.getItem("windowLongitude");
+        }
+        else {
+            var currentLatitude = windowLatitude;
+            var currentLongitude = windowLongitude;
+        }
+        var myUserInput = new userInput(keyword, startDate, endDate, location, currentLatitude, currentLongitude);
+        var mytmEventList = tmEventSearch(myUserInput);
+        if (localStorage.getItem("tmEventListString") === null) {
+            var mytmEventListString = JSON.stringify(mytmEventList);
+            localStorage.setItem("tmEventListString", mytmEventListString);
+        }
+        else {
+            localStorage.removeItem("tmEventListString");
+            var mytmEventListString = JSON.stringify(mytmEventList);
+            localStorage.setItem("tmEventListString", mytmEventListString);
+        }
+        displaySearchResult(mytmEventList);
+
+    });
+})
+$("#btnReset").on("click", function (event) {
+    event.preventDefault();
+    $("#displayResults").empty();
+    $("#keyword").val("");
+    $("#startDate").val("");
+    $("#endDate").val("");
+    $("#location").val("");
+    localStorage.removeItem("tmEventListString");
+    window.location.href = "main.html";
+})
 
 // this function using regular expression to find out the user location input is city or zip code.
 function isCity(location) {
